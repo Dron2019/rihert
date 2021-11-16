5b5170a1a313326f73686fdb54db0999374a0e51
Dron2019 <mr.andrey@bigmir.net>
2021-11-16 17:10:20 +0200

    try to add transfer from hor scroll to scaled section

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -875,6 +875,26 @@ gsap.timeline({
   scrollTrigger:  {
     scrub: true,
     scroller: pageContainer,
+    trigger: ".screen5",
+    start: "0 bottom",
+    end: `${innerHeight} bottom`,
+    onLeaveBack: () => {
+      gsap.timeline().to('.screen5__inner', { autoAlpha: 0, duration: 0.1 })
+        .to('#sectionPin', { autoAlpha: 1, duration: 0.1 }, '<')
+    },
+    onEnter: () => {
+      gsap.timeline().to('.screen5__inner', { autoAlpha: 1, duration: 0.1 })
+        .to('#sectionPin', { autoAlpha: 0, duration: 0.1 }, '<')
+
+    }
+  }
+})
+.from('.screen5__inner', { scale: 2.7, y: '-100vh', transformOrigin: '100% 0' })
+gsap.timeline({
+  ease: 'none',
+  scrollTrigger:  {
+    scrub: true,
+    scroller: pageContainer,
     trigger: ".screen7",
     // start: "-200px bottom",
     end: '500px bottom',
