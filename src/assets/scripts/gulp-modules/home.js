cd5429ac66451446b0eac182e0337ff9fa820693
Dron2019 <mr.andrey@bigmir.net>
2021-11-17 14:41:37 +0200

    mobile continue

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -34,43 +34,7 @@ window.screen2To1Tl = gsap.timeline({ paused: true })
   .to('.screen1', {
     scale: 1
   }, '+1.5')
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
 
 // disableScroll();
 const pageContainer = document.querySelector(".scroller-container");
@@ -138,6 +102,10 @@ function stopCustomScroll() {
 function startCustomScroll() {
   scroller.start();
 }
+const isMobile = () => window.matchMedia('(max-width: 575px)').matches;
+
+
+console.log(isMobile);
 ////////////////////////////////////
 ////////////////////////////////////
   // let pinWrap = document.querySelector(".pin-wrap");
